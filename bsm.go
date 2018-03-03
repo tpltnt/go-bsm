f223861856780c4ed0b723f6650278d078a47794
tpltnt <tpltnt.github@dropcut.net>
2018-03-03 21:29:17 +0100

    inital commit

---
+++ bsm.go
@@ -0,0 +1,28 @@
+// Parse BSM files
+package main
+
+import (
+	"flag"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+)
+
+func main() {
+	// handle CLI
+	flag.String("auditfile", "", "FreeBSD audit file to parse")
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+
+	// open file to process
+	aFilePath := viper.GetString("auditfile")
+	if 0 != len(aFilePath) {
+		file, err := os.Open(aFilePath)
+		if err != nil {
+			log.Fatal("Could not open input file", err)
+		}
+		defer file.Close()
+	}
+}
